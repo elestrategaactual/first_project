7b2e877428e1958562a83453c0b98795e19ac2aa
elestrategaactual <51063748+elestrategaactual@users.noreply.github.com>
2023-04-17 23:22:15 +0200

    add the runge kutta code

--- src/odom_node.cpp
+++ src/odom_node.cpp
@@ -23,7 +23,7 @@ std_msgs::Float64 m2;
 	double v_x;
 	double v_y;
 	double w_center;
-	double d= 2.8;
+	double d= 2.8;// distance betwen axes
 	
 	double x;
 	double y;
@@ -32,6 +32,9 @@ std_msgs::Float64 m2;
 	double x_b;
 	double y_b;
 	double th_b;
+
+	double dt =0.1; 
+
 
 private:
 ros::NodeHandle n; 
@@ -80,12 +83,11 @@ public:
 	// END DEFINITION ALL PUBLISH TOPICS
 
 	// START DEFINITION ALL TIMERS
-	timer1 = n.createTimer(ros::Duration(0.1), &odometry::callback1, this);
+	timer1 = n.createTimer(ros::Duration(dt), &odometry::callback1, this);
 	// END DEFINITION ALL TIMERS
 		
 	// tf
-	tf::TransformBroadcaster br;
-
+	
 	// START GET STATIC PARAMS
 	n.getParam("/starting_x", x);
 	n.getParam("/starting_y", y);
@@ -102,21 +104,61 @@ speed_angle=*msg;
 void callback1(const ros::TimerEvent& ev)
 {
 	//for debug TRY TO READ THE BAG FILE
-	m1.data=speed_angle.x;
-	m2.data=speed_angle.y;
+	odometry_calc();
+	m1.data=x;
+	m2.data=y;
 	speed.publish(m1);
 	angle.publish(m2);
 	
+	tf::TransformBroadcaster br;
 	tf::Transform transform;
-    	transform.setOrigin( tf::Vector3(x_b, y_b, 0) );
-    	tf::Quaternion q;
-    	q.setRPY(0, 0, th_b);
-    	transform.setRotation(q);
-    	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "vehicle_centre"));
+	tf::Quaternion q;
+    transform.setOrigin( tf::Vector3(x_b, y_b, 0) );
+    q.setRPY(0, 0, th_b);
+	transform.setRotation(q);
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "vehicle_centre"));
 	
   	ROS_INFO("Callback 1 triggered");
 	//for debug TRY TO READ THE BAG FILE
 }
+
+
+double dx(float x1, float y1, float th1){
+	return(speed_angle.x*cos(th1)); // speed of the back wheel
+}
+
+double dy(float x1, float y1, float th1){
+	return (speed_angle.x*sin(th1)); // speed of the back wheel
+}
+
+double dth( float x1, float y1, float th1){
+	return	(speed_angle.x*tan(speed_angle.y)/d ); // speed of the back wheel
+}
+void rngkutta(float h){
+	float k1=h* dx(x,y,th);
+	float l1=h* dy(x,y,th);
+	float m1=h* dth(x,y,th);
+
+	float k2=h* dx(x+1/2*k1,y+1/2*l1,th+1/2*m1);
+	float l2=h* dy(x+1/2*k1,y+1/2*l1,th+1/2*m1);
+	float m2=h* dth(x+1/2*k1,y+1/2*l1,th+1/2*m1);
+
+	float k3=h* dx(x+1/2*k2,y+1/2*l2,th+1/2*m2);
+	float l3=h* dy(x+1/2*k2,y+1/2*l2,th+1/2*m2);
+	float m3=h* dth(x+1/2*k2,y+1/2*l2,th+1/2*m2);
+
+	float k4=h* dx(x+k3,y+l3,th+m3);
+	float l4=h* dy(x+k3,y+l3,th+m3);
+	float m4=h* dth(x+k3,y+l3,th+m3);
+
+	x=x+(1/6)*(k1+2*k2+2*k3+k4);
+	y=y+(1/6)*(l1+2*l2+2*l3+l4);
+	th=th+(1/6)*(m1+2*m2+2*m3+m4);
+}
+
+
+
+
 
 void odometry_calc(){
 	w_center=speed_angle.x*cos(speed_angle.y*tan(speed_angle.y)/d);
@@ -124,6 +166,7 @@ void odometry_calc(){
 	v_x=w_center*d/tan(speed_angle.y);
 	
 	//HERE PUT THE ODEMETRY CALCULATION
+	rngkutta(dt);
 }
 
 bool reset(first_project::reset_odom::Request  &req,
